6acb9c36037b00e321d89aaefb67cf478fa28009
youli <youliso@hotmail.com>
2025-01-15 12:01:20 +0800

    refactor(src): 重构 get_files 和 set_files 函数
    - 改进错误处理，确保在出现错误时返回空数组或抛出错误
    - 添加输入验证，防止空文件列表被传递给 set_files
    - 优化代码结构，提高可读性和可维护性
    - 移除无效的测试代码

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -6,7 +6,3 @@ test("getFiles", (t) => {
   const files = getFiles();
   t.true(Array.isArray(files), "getFiles should return an array");
 });
-
-test("setFiles", (t) => {
-  t.true(setFiles(["C:\\Users\\606\\Downloads\\neovide.msi"]), "setFiles is error");
-});
--- src/lib.rs
+++ src/lib.rs
@@ -4,32 +4,52 @@
 extern crate napi_derive;
 
 use clipboard_rs::{Clipboard, ClipboardContext, ContentFormat};
+use napi::Error;
 
 #[napi]
 pub fn get_files() -> Vec<String> {
-  let ctx = ClipboardContext::new().unwrap();
-  if ctx.has(ContentFormat::Files) {
-    let files = ctx.get_files();
-    match files {
-      Ok(files) => files,
-      Err(err) => {
-        eprintln!("Failed to get files from clipboard: {}", err);
+  match ClipboardContext::new() {
+    Ok(ctx) => {
+      if ctx.has(ContentFormat::Files) {
+        match ctx.get_files() {
+          Ok(files) => files,
+          Err(err) => {
+            eprintln!("Failed to get files from clipboard: {}", err);
+            vec![]
+          }
+        }
+      } else {
+        println!("Clipboard does not contain files.");
         vec![]
       }
     }
-  } else {
-    vec![]
+    Err(err) => {
+      eprintln!("Failed to create clipboard context: {}", err);
+      vec![]
+    }
   }
 }
 
 #[napi]
-pub fn set_files(files: Vec<String>) -> bool {
-  let ctx = ClipboardContext::new().unwrap();
+pub fn set_files(files: Vec<String>) -> Result<bool, Error> {
+  // 基本输入验证
+  if files.is_empty() {
+    return Err(Error::from_reason(format!("Failed to files empty")));
+  }
+
+  // 创建 ClipboardContext 并处理可能的错误
+  let ctx = ClipboardContext::new()
+    .map_err(|err| Error::from_reason(format!("Failed to create ClipboardContext: {}", err)))?;
+
+  // 设置文件到剪贴板并处理可能的错误
   match ctx.set_files(files) {
-    Ok(_) => true,
+    Ok(_) => Ok(true),
     Err(err) => {
       eprintln!("Failed to set files to clipboard: {}", err);
-      false
+      Err(Error::from_reason(format!(
+        "Failed to set files to clipboard: {}",
+        err
+      )))
     }
   }
 }
