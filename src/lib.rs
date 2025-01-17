13bb0cd2ba9b8ac3c8a128a655ba3178684a88f6
youli <youliso@hotmail.com>
2025-01-17 17:31:34 +0800

    refactor(set_files): 重构 set_files 函数的错误处理
    - 移除了 napi::Error 类型的引用
    - 将函数返回类型从 Result<bool, Error> 改为 bool
    - 简化了错误处理逻辑，使用 eprintln! 输出错误信息
    - 优化了代码结构，减少了嵌套的 match 表达式

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@
 extern crate napi_derive;
 
 use clipboard_rs::{Clipboard, ClipboardContext, ContentFormat};
-use napi::Error;
 
 #[napi]
 pub fn get_files() -> Vec<String> {
@@ -31,25 +30,23 @@ pub fn get_files() -> Vec<String> {
 }
 
 #[napi]
-pub fn set_files(files: Vec<String>) -> Result<bool, Error> {
+pub fn set_files(files: Vec<String>) -> bool {
   // 基本输入验证
   if files.is_empty() {
-    return Err(Error::from_reason(format!("Failed to files empty")));
+    return false;
   }
 
-  // 创建 ClipboardContext 并处理可能的错误
-  let ctx = ClipboardContext::new()
-    .map_err(|err| Error::from_reason(format!("Failed to create ClipboardContext: {}", err)))?;
-
-  // 设置文件到剪贴板并处理可能的错误
-  match ctx.set_files(files) {
-    Ok(_) => Ok(true),
+  match ClipboardContext::new() {
+    Ok(ctx) => match ctx.set_files(files) {
+      Ok(_) => true,
+      Err(err) => {
+        eprintln!("Failed to set files to clipboard: {}", err);
+        false
+      }
+    },
     Err(err) => {
-      eprintln!("Failed to set files to clipboard: {}", err);
-      Err(Error::from_reason(format!(
-        "Failed to set files to clipboard: {}",
-        err
-      )))
+      eprintln!("Failed to create clipboard context: {}", err);
+      false
     }
   }
 }
